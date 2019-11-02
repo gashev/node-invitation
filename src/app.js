d8df862764ae775eb221d5b8be05d6fdfd6ff690
Oleg Gashev <oleg@gashev.net>
2019-11-02 15:32:44 -0400

    app.js code refactoring.

--- src/app.js
+++ src/app.js
@@ -2,18 +2,14 @@ const express = require('express');
 const LeaderElection = require('../src/leader-election');
 
 exports.initApp = function(ip, port, servers) {
-    const node = new LeaderElection.Node(`${ip}:${port}`, servers);
     const app = express();
+    const node = new LeaderElection.Node(`${ip}:${port}`, servers);
+
     app.use(express.json())
 
-    app.get('/status', function(req, res) {
-        console.log('/status request');
-        res.send(node.statusRequest());
-    });
-
-    app.get('/ping', function(req, res) {
-        console.log('/ping request');
-        res.send(node.pingRequest());
+    app.post('/accept', async function(req, res) {
+        console.log('/accept request');
+        res.send(await node.acceptRequest(req.body));
     });
 
     app.post('/merge', async function(req, res) {
@@ -21,9 +17,14 @@ exports.initApp = function(ip, port, servers) {
         res.send(await node.mergeRequest(req.body));
     });
 
-    app.post('/accept', async function(req, res) {
-        console.log('/accept request');
-        res.send(await node.acceptRequest(req.body));
+    app.get('/ping', function(req, res) {
+        console.log('/ping request');
+        res.send(node.pingRequest());
+    });
+
+    app.get('/status', function(req, res) {
+        console.log('/status request');
+        res.send(node.statusRequest());
     });
 
     app.listen(port, ip, () => console.log(`Example app listening on port ${port}!`));
